58214109c48e4fb2c42bbb5e333f68c050b517d8
ZhangKe <kezhang404@gmail.com>
2024-10-12 18:00:04 +0800

    fixed entity not serializable

--- src/commonMain/kotlin/com/zhangke/activitypub/entities/ActivityPubMarkersEntity.kt
+++ src/commonMain/kotlin/com/zhangke/activitypub/entities/ActivityPubMarkersEntity.kt
@@ -1,12 +1,15 @@
 package com.zhangke.activitypub.entities
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ActivityPubMarkersEntity(
-    val notifications: Marker?,
-    val home: Marker?,
+    val notifications: Marker? = null,
+    val home: Marker? = null,
 ) {
 
+    @Serializable
     data class Marker(
         @SerialName("last_read_id")
         val lastReadId: String,
